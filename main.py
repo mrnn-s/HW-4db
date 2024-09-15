9e2d83b7a0eba8514b2567f2accf4f931a3ea0ae
MariannaSarkisian <spn.mjj@gmail.com>
2024-09-15 21:37:06 +0300

    start4hw

---
+++ main.py
@@ -0,0 +1,10 @@
+#User-Agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36
+
+
+from lxml import html
+import requests
+from pprint import pprint
+import csv
+
+
+
